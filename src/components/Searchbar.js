2debf4131da2507e859f478d0b5d766b8992fdff
brutal-kitten <olgaprudyus@gmail.com>
2018-10-30 18:03:41 +0100

    feat: add input form

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -2,11 +2,39 @@ import React, { Component } from 'react';
 
 class Searchbar extends Component {
 
+  state = {
+    query: ''
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  }
+
+  /*update state*/
+  updateQuery = (value) => {
+    this.setState({query: value});
+  }
+
+  /*clear query and update state*/
+  clearQuery = () => {
+    this.setState({query: ''});
+  }
+
   render () {
 
     return (
       <div className="searchbar">
-      <h2>Find your favorite video</h2>
+        <h2>Find your favorite video</h2>
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          Enter video URL or identifier: <br/>
+          <input type="text"
+            placeholder= "Let's find a video"
+            value={this.state.query}
+            onChange={(event) => this.updateQuery(event.target.value)}
+          />
+          <button>Submit</button>
+        </form>
       </div>
     )
   }

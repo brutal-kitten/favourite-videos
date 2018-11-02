a5c6a48360bd67fafe5d1b89512302ed97b05d87
brutal-kitten <olgaprudyus@gmail.com>
2018-11-02 13:05:05 +0100

    feat: get and add info about video in infobox

---
+++ src/components/Infobox.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
+class Infobox extends Component {
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    likes: PropTypes.string.isRequired,
+    views: PropTypes.string.isRequired
+  }
+
+
+  render() {
+
+
+
+    return (
+      <div className="infobox">
+        <div className="title"> Title: <span>{this.props.title}</span></div>
+        <div className="addedAt">Published At: <span> {this.props.date}</span></div>
+        <div className="likes"><span>{this.props.likes}</span> Likes</div>
+        <div className="views"><span>{this.props.views}</span> Views</div>
+      </div>
+    )
+  }
+
+}
+
+export default Infobox;
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Infobox from './Infobox';
 
 
 class VideoContainer extends Component {
 
   state = {
-    info: {},
-    fetchAgain: true
+    info: [],
+    fetchAgain: true,
+    title: '',
+    date: '',
+    likes: '',
+    views: ''
   }
 
   componentDidMount() {
@@ -13,10 +18,14 @@ class VideoContainer extends Component {
       fetch(`https://www.googleapis.com/youtube/v3/videos?id=hY7m5jjJ9mM&key=AIzaSyB7asSzTvcMogycBslu8o4RB3DjOumaqtA&part=snippet,contentDetails,statistics,status`)
         .then((response) => response.json())
         .then((result) => {
-          console.log(result.items);
+          console.log(result.items[0]);
           this.setState({
             fetchAgain: false,
-            info: result.items
+            info: result.items,
+            title: result.items[0].snippet.title,
+            date: result.items[0].snippet.publishedAt,
+            views: result.items[0].statistics.viewCount,
+            likes: result.items[0].statistics.likeCount
           });
         });
       }
@@ -26,7 +35,12 @@ class VideoContainer extends Component {
 
     return (
         <div className="container">
-
+          <Infobox
+            title={this.state.title}
+            date={this.state.date}
+            views={this.state.views}
+            likes={this.state.likes}
+          />
         </div>
     )
   }

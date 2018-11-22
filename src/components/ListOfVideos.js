39bb2a781c6f611306b22a9c01705261cbb58916
brutal-kitten <olgaprudyus@gmail.com>
2018-11-22 13:55:40 +0100

    feat: ad rules for video containers for all size

--- src/components/ListOfVideos.js
+++ src/components/ListOfVideos.js
@@ -20,23 +20,25 @@ class ListOfVideos extends Component {
           showFav={this.props.showFav}
           returnToList={this.props.returnToList}
         />
-        {(this.props.list.map((item) => (
-        <VideoContainer
-          key={item.id}
-          id={item.id}
-          title={item.title}
-          date={item.date}
-          views={item.views}
-          likes={item.likes}
-          thumbnails={item.thumbnails}
-          isfavorite={item.favorite}
-          removeFromFavorite={this.props.removeFromFavorite}
-          addToFavorite={this.props.addToFavorite}
-          deleteVideo={this.props.deleteVideo}
-          playVideo={this.props.playVideo}
-          changeSearchResultError={this.props.changeSearchResultError}
-        />
-      )))}
+        <div className="listContainer">
+          {(this.props.list.map((item) => (
+            <VideoContainer
+              key={item.id}
+              id={item.id}
+              title={item.title}
+              date={item.date}
+              views={item.views}
+              likes={item.likes}
+              thumbnails={item.thumbnails}
+              isfavorite={item.favorite}
+              removeFromFavorite={this.props.removeFromFavorite}
+              addToFavorite={this.props.addToFavorite}
+              deleteVideo={this.props.deleteVideo}
+              playVideo={this.props.playVideo}
+              changeSearchResultError={this.props.changeSearchResultError}
+            />
+          )))}
+        </div>
       </div>
     )
   }

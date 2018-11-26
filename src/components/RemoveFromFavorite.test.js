7559db56de8cc2de727b7fcc15c31e74e8857a21
brutal-kitten <olgaprudyus@gmail.com>
2018-11-26 15:50:48 +0100

    test: add tests to RemoveFromFavorite component

---
+++ src/components/RemoveFromFavorite.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RemoveFromFavorite from './RemoveFromFavorite.js';
+import TestRenderer from 'react-test-renderer';
+import { shallow, mount, render } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+it('should display a button', () => {
+  const tree = TestRenderer.create(
+    <div className="removeFromFav" >
+      <button tabIndex='0' className="buttonRemoveFromFav" type="button" onClick={(event) => this.handleClick(event)}>
+        <span className="glyphicon glyphicon-star"></span>
+      </button>
+    </div>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+describe ('shoud call handleclick function after click', () => {
+  const mockRemoveFromFavorite = jest.fn();
+  const mockTrigger = jest.fn();
+  const props = {
+    removeFromFavorite: mockRemoveFromFavorite,
+    trigger: mockTrigger,
+    id: "12345"
+  };
+
+  const wrapper = shallow(<RemoveFromFavorite {...props}/>);
+
+  wrapper.find('button').simulate('click', {
+    preventDefault: () => {},
+  });
+
+  it('should call removeFromFavorite function with 1 argument - id', () => {
+    expect(mockRemoveFromFavorite.mock.calls.length).toBe(1);
+    expect(mockRemoveFromFavorite.mock.calls[0][0]).toBe("12345");
+  });
+
+  it('should call trigger function', () => {
+    expect(mockTrigger.mock.calls.length).toBe(1);
+  });
+
+})
